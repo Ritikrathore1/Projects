3d60d430127a830926e2872594304a7d2cb7e215
Ritikrathore1 <86469310+Ritikrathore1@users.noreply.github.com>
2021-06-25 15:23:26 +0530

    Add files via upload

---
+++ BAND NAME GENERATOR P1.py
@@ -0,0 +1,9 @@
+#BAND NAME GENERATOR
+#PROJECT=1
+
+print("welcome to the band name generator! \n")
+
+city = input("Enter the city in which you grew up in? \n")
+pet = input("What's your first pet name?\n")
+print("Your band name should be " +" "+ city +" "+pet)
+
